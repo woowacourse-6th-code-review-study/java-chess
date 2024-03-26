07192b30c9a9505dbcf36e4db8cb478d52bd0467
robinjoon <robin980108@naver.com>
2024-03-26 12:08:51 +0900

    fix: 명령어 예시 출력 수정

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -12,7 +12,8 @@ public class OutputView {
         System.out.printf("> 체스 게임을 시작합니다.%n"
                 + "> 게임 시작 : start%n"
                 + "> 게임 종료 : end%n"
-                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n");
+                + "> 게임 이동 : move source위치 target위치 - 예. move b2 b3%n"
+                + "> 현재 점수 확인 : status%n");
     }
 
     public static void printChessBoard(List<PieceDTO> pieceDTOS) {
