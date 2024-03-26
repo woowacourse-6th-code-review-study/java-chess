a1e44b40f65d75412819c67f1b841efd20a5593b
robinjoon <robin980108@naver.com>
2024-03-26 11:07:07 +0900

    refactor: 각 팀의 포인트를 계산하는 책임을 ChessBoard 로부터 분리

--- src/main/java/chess/domain/board/ChessBoard.java
+++ src/main/java/chess/domain/board/ChessBoard.java
@@ -10,9 +10,7 @@ import chess.domain.piece.Team;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class ChessBoard {
     private final List<Piece> piecesOnBoard;
@@ -107,36 +105,6 @@ public class ChessBoard {
     }
 
     double calculatePoint(Team team) {
-        double totalPoint = calculateWithOutSameColumnPawn(team);
-        Map<Integer, List<Piece>> pawnsAtSameColumn = pawnGroupingByColumn(team);
-        double correctionPoint = calculatePawnCorrectionPoint(pawnsAtSameColumn);
-        return totalPoint - correctionPoint;
-    }
-
-    private double calculateWithOutSameColumnPawn(Team team) {
-        return piecesOnBoard.stream()
-                .filter(piece -> piece.isTeamWith(team))
-                .mapToDouble(Piece::getPoint)
-                .reduce(0.0, Double::sum);
-    }
-
-    private Map<Integer, List<Piece>> pawnGroupingByColumn(Team team) {
-        return piecesOnBoard.stream()
-                .filter(piece -> piece.isTeamWith(team))
-                .filter(this::isPawn)
-                .collect(Collectors.groupingBy(Piece::getColumn, Collectors.toUnmodifiableList()));
-    }
-
-    private boolean isPawn(Piece piece) {
-        PieceType pieceType = piece.getPieceType();
-        return pieceType.equals(PieceType.PAWN);
-    }
-
-    private double calculatePawnCorrectionPoint(Map<Integer, List<Piece>> pawnsAtSameColumn) {
-        final double correctionPointPerPawn = 0.5;
-        return pawnsAtSameColumn.values().stream()
-                .filter(pieces -> pieces.size() > 1)
-                .mapToDouble(pieces -> pieces.size() * correctionPointPerPawn)
-                .reduce(0.0, Double::sum);
+        return PointCalculator.calculatePoint(team, piecesOnBoard);
     }
 }
