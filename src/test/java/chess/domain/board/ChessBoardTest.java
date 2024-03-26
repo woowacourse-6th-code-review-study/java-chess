1ecd8713c2b40e8303e62976badfea3a37407e54
robinjoon <robin980108@naver.com>
2024-03-26 11:56:02 +0900

    test: 잘못된 테스트 수정

--- src/test/java/chess/domain/board/ChessBoardTest.java
+++ src/test/java/chess/domain/board/ChessBoardTest.java
@@ -6,6 +6,7 @@ import static chess.domain.Position.A3;
 import static chess.domain.Position.A4;
 import static chess.domain.Position.A5;
 import static chess.domain.Position.A6;
+import static chess.domain.Position.A8;
 import static chess.domain.Position.B1;
 import static chess.domain.Position.B2;
 import static chess.domain.Position.B6;
@@ -24,6 +25,7 @@ import static chess.domain.Position.E1;
 import static chess.domain.Position.E3;
 import static chess.domain.Position.F2;
 import static chess.domain.Position.H1;
+import static chess.domain.Position.H2;
 import static chess.domain.piece.PieceMoveResult.FAILURE;
 import static chess.domain.piece.PieceMoveResult.SUCCESS;
 import static chess.domain.piece.Team.BLACK;
@@ -62,7 +64,7 @@ class ChessBoardTest {
 
     public static Stream<Arguments> moveFailureCauseInvalidMoveParameters() {
         return Stream.of(
-                Arguments.of(D2, D5), Arguments.of(D2, C3), Arguments.of(A1, H1), Arguments.of(A1, B6),
+                Arguments.of(D2, D5), Arguments.of(D2, C3), Arguments.of(A1, H2), Arguments.of(A1, B6),
                 Arguments.of(B1, B2), Arguments.of(C1, C6), Arguments.of(D1, D3), Arguments.of(D1, C3),
                 Arguments.of(E1, E3)
         );
@@ -79,7 +81,8 @@ class ChessBoardTest {
     @MethodSource("moveSuccessParameters")
     @DisplayName("정상적인 경우에 이동이 성공하는지 검증")
     void moveSuccess(Position from, Position to) {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE), new Rook(A1, WHITE), new King(E1, WHITE),
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE),
+                new Rook(A1, WHITE), new King(E1, WHITE),
                 new Knight(B1, WHITE), new Bishop(C1, WHITE), new Queen(D1, WHITE));
         PieceMoveResult moveResult = chessBoard.move(from, to);
         Assertions.assertThat(moveResult).isEqualTo(SUCCESS);
@@ -89,7 +92,8 @@ class ChessBoardTest {
     @MethodSource("moveFailureCauseInvalidMoveParameters")
     @DisplayName("해당 위치가 이동이 불가능한 위치라서 이동이 실패하는지 검증")
     void moveFailureCauseInvalidMove(Position from, Position to) {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE), new Rook(A1, WHITE), new King(E1, WHITE),
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE),
+                new Rook(A1, WHITE), new King(E1, WHITE),
                 new Knight(B1, WHITE), new Bishop(C1, WHITE), new Queen(D1, WHITE));
         PieceMoveResult moveResult = chessBoard.move(from, to);
         Assertions.assertThat(moveResult).isEqualTo(FAILURE);
@@ -98,7 +102,7 @@ class ChessBoardTest {
     @Test
     @DisplayName("해당 위치에 말이 없어서 이동이 실패하는지 검증")
     void moveFailureCausePieceNotFound() {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE));
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE));
         PieceMoveResult moveResult = chessBoard.move(D3, D4);
         Assertions.assertThat(moveResult).isEqualTo(FAILURE);
     }
@@ -106,7 +110,8 @@ class ChessBoardTest {
     @Test
     @DisplayName("팀의 이동 차례가 아니라서 이동이 실패하는지 검증")
     void moveFailureCauseInvalidTurn() {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE), new Pawn(D7, BLACK));
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE),
+                new Pawn(D7, BLACK));
         chessBoard.move(D2, D4);
 
         PieceMoveResult moveResult = chessBoard.move(D4, D5);
@@ -117,17 +122,20 @@ class ChessBoardTest {
     @DisplayName("상대 말을 잡으면 그 말이 보드에서 사라지는지 검증")
     void catchSuccess() {
         Pawn survivor = new Pawn(D2, WHITE);
-        ChessBoard chessBoard = new ChessBoard(survivor, new Pawn(C3, BLACK));
+        King whiteKing = new King(A8, WHITE);
+        King blackKing = new King(H1, BLACK);
+        ChessBoard chessBoard = new ChessBoard(whiteKing, blackKing, survivor, new Pawn(C3, BLACK));
         chessBoard.move(D2, C3);
         List<Piece> piecesOnBoard = chessBoard.getPiecesOnBoard();
         Assertions.assertThat(piecesOnBoard)
-                .containsExactly(survivor);
+                .containsExactly(whiteKing, blackKing, survivor);
     }
 
     @Test
     @DisplayName("잘못된 이동을 시도했을 때 차례가 넘어가지 않는지 검증")
     void validateTurnNotChangeWhenInvalidMove() {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE), new Pawn(C3, BLACK));
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE),
+                new Pawn(C3, BLACK));
         chessBoard.move(D2, D8);
         PieceMoveResult moveResult = chessBoard.move(C3, C2);
         Assertions.assertThat(moveResult).isEqualTo(FAILURE);
@@ -136,7 +144,8 @@ class ChessBoardTest {
     @Test
     @DisplayName("이동이 성공했을 때 차례가 넘어가는지 검증")
     void validateTurnChangeWhenValidMove() {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(D2, WHITE), new Pawn(C3, BLACK));
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(D2, WHITE),
+                new Pawn(C3, BLACK));
         chessBoard.move(D2, D3);
         PieceMoveResult moveResult = chessBoard.move(C3, C2);
         Assertions.assertThat(moveResult).isEqualTo(SUCCESS);
@@ -156,7 +165,7 @@ class ChessBoardTest {
     @Test
     @DisplayName("해당 위치에 말이 없는 경우 팀을 판단할 수 없는지 검증")
     void whichTeamWhenEmpty() {
-        ChessBoard chessBoard = new ChessBoard(List.of());
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK));
         Optional<Team> actual = chessBoard.whichTeam(D2);
         Assertions.assertThat(actual)
                 .isEmpty();
@@ -178,7 +187,8 @@ class ChessBoardTest {
     @Test
     @DisplayName("같은 열의 폰이 포함된 각 팀의 점수가 잘 계산되는지 검증")
     void calculatePointWithPawn() {
-        ChessBoard chessBoard = new ChessBoard(new Pawn(A2, BLACK), new Pawn(B2, BLACK), new Pawn(A3, BLACK),
+        ChessBoard chessBoard = new ChessBoard(new King(A8, WHITE), new King(H1, BLACK), new Pawn(A2, BLACK),
+                new Pawn(B2, BLACK), new Pawn(A3, BLACK),
                 new Pawn(A4, WHITE), new Pawn(A5, WHITE), new Pawn(A6, WHITE));
         double whiteTeamPoint = chessBoard.calculatePoint(WHITE);
         double blackTeamPoint = chessBoard.calculatePoint(BLACK);
