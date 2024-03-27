72d8cf8e8fcad2af60e329cfa02ee1ebb9b30ebf
libienz <leekh10195@gmail.com>
2024-03-27 11:12:09 +0900

    feat: 보드의 책임과 체스보드의 검증 책임 분리

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -17,10 +17,7 @@ public class ChessGame {
     }
 
     public void move(Position start, Position destiantion) {
-        validatePieceExist(start);
         validateTurn(chessBoard.findPieceByPosition(start));
-        validateLegalMovement(start, destiantion);
-
         chessBoard.move(start, destiantion);
         turn = turn.otherTeam();
     }
@@ -29,21 +26,9 @@ public class ChessGame {
         return chessBoard;
     }
 
-    private void validatePieceExist(Position start) {
-        if (chessBoard.positionIsEmpty(start)) {
-            throw new IllegalArgumentException("움직임을 시작하는 위치에 기물이 존재하지 않습니다");
-        }
-    }
-
     private void validateTurn(Piece piece) {
         if (piece.isOtherTeam(turn)) {
             throw new IllegalArgumentException(turn + "의 차례입니다");
-        }
-    }
-
-    private void validateLegalMovement(Position start, Position destination) {
-        if (!chessBoard.canMove(start, destination)) {
-            throw new IllegalArgumentException("기물의 행마법에 어긋나는 움직임입니다");
         }
     }
 }
