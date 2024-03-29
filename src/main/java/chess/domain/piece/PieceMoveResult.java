854e4fe8fa0307a212657df2efc5a6e04caefc6b
robinjoon <robin980108@naver.com>
2024-03-29 17:39:53 +0900

    chore: 삭제하지 않은 ToDo 삭제

--- src/main/java/chess/domain/piece/PieceMoveResult.java
+++ src/main/java/chess/domain/piece/PieceMoveResult.java
@@ -1,9 +1,8 @@
 package chess.domain.piece;
 
 public enum PieceMoveResult {
-    //Todo 게임 종료 여부 분리
     SUCCESS, FAILURE, CATCH, WHITE_WIN, BLACK_WIN;
-    
+
     public boolean isEnd() {
         return this.equals(BLACK_WIN) || this.equals(WHITE_WIN);
     }
