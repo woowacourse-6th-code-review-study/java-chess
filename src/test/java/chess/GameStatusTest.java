6017958b0edad288e207a28ba0c3f55621e7a090
hyxrxn <gpfls981220@gmail.com>
2024-03-30 22:38:13 +0900

    test(GameStatus): 이긴 팀을 이용해 게임 상태 확인

--- src/test/java/chess/GameStatusTest.java
+++ src/test/java/chess/GameStatusTest.java
@@ -3,10 +3,20 @@ package chess;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import chess.domain.piece.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameStatusTest {
+
+    @Test
+    @DisplayName("이긴 팀으로 해당 상태를 판단한다.")
+    void winByWhichTeamTest() {
+        assertAll(
+                () -> assertThat(GameStatus.winBy(Team.BLACK)).isEqualTo(GameStatus.BLACK_WIN),
+                () -> assertThat(GameStatus.winBy(Team.WHITE)).isEqualTo(GameStatus.WHITE_WIN)
+        );
+    }
 
     @Test
     @DisplayName("플레이 중인 상태인지 확인한다.")
