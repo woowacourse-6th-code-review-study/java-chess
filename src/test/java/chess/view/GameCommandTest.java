0b14727111d5bb755fae379d083e7a5c5643ac50
hyxrxn <gpfls981220@gmail.com>
2024-03-30 22:27:16 +0900

    test(GameCommand): 해당 커맨드 확인

--- src/test/java/chess/view/GameCommandTest.java
+++ src/test/java/chess/view/GameCommandTest.java
@@ -10,6 +10,25 @@ import org.junit.jupiter.api.Test;
 class GameCommandTest {
 
     @Test
+    @DisplayName("해당하는 커맨드를 찾는다.")
+    void findCommandFromStringTest() {
+        assertAll(
+                () -> assertThat(GameCommand.from("start")).isEqualTo(GameCommand.START),
+                () -> assertThat(GameCommand.from("end")).isEqualTo(GameCommand.END),
+                () -> assertThat(GameCommand.from("move")).isEqualTo(GameCommand.MOVE),
+                () -> assertThat(GameCommand.from("status")).isEqualTo(GameCommand.STATUS)
+        );
+    }
+
+    @Test
+    @DisplayName("해당하는 커맨드가 없으면 예외를 발생시킨다.")
+    void findCommandFromStringTest_whenNotExist() {
+        assertThatThrownBy(() -> GameCommand.from("notExistCommand"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 커멘드입니다.");
+    }
+
+    @Test
     @DisplayName("게임 시작 전 불가능한 커맨드인지 확인한다.")
     void isImpossibleCommandBeforeStartGameTest() {
         assertAll(
