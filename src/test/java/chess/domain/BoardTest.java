16a3341d0a953891b3e8ea96fbef60319568dd6b
hyxrxn <gpfls981220@gmail.com>
2024-03-31 04:19:52 +0900

    test(Board): 특정 팀의 말 및 폰의 위치 확인

--- src/test/java/chess/domain/BoardTest.java
+++ src/test/java/chess/domain/BoardTest.java
@@ -4,9 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
 import chess.domain.piece.Queen;
+import chess.domain.piece.Rook;
 import chess.domain.piece.Team;
 import chess.domain.position.File;
 import chess.domain.position.Position;
@@ -102,5 +106,72 @@ class BoardTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("다른 말이 있어 이동 불가능합니다.");
         }
+    }
+
+    @Test
+    @DisplayName("특정 팀의 말들을 알 수 있다.")
+    void getPiecesOfTest() {
+        Queen queen = new Queen(Team.WHITE);
+        Rook rook = new Rook(Team.WHITE);
+        Bishop bishop = new Bishop(Team.WHITE);
+        Knight knight = new Knight(Team.BLACK);
+        Pawn pawn = new Pawn(Team.BLACK);
+        King king = new King(Team.BLACK);
+        Map<Position, Piece> boardMap = Map.of(
+                new Position(File.B, Rank.TWO), queen,
+                new Position(File.F, Rank.TWO), rook,
+                new Position(File.F, Rank.SIX), bishop,
+                new Position(File.D, Rank.THREE), knight,
+                new Position(File.E, Rank.SIX), pawn,
+                new Position(File.A, Rank.FOUR), king);
+        Board board = new Board(boardMap);
+
+        assertAll(
+                () -> assertThat(board.getPiecesOf(Team.WHITE).toList())
+                        .containsExactlyInAnyOrder(queen, rook, bishop),
+                () -> assertThat(board.getPiecesOf(Team.BLACK).toList())
+                        .containsExactlyInAnyOrder(knight, pawn, king)
+        );
+    }
+
+    @Test
+    @DisplayName("특정 팀의 폰의 위치들을 알 수 있다.")
+    void getPawnPositionsOfTest() {
+        Map<Position, Piece> boardMap = new java.util.HashMap<>(Map.of(
+                new Position(File.B, Rank.EIGHT), new King(Team.BLACK),
+                new Position(File.C, Rank.EIGHT), new Rook(Team.BLACK),
+                new Position(File.A, Rank.SEVEN), new Pawn(Team.BLACK),
+                new Position(File.C, Rank.SEVEN), new Pawn(Team.BLACK),
+                new Position(File.D, Rank.SEVEN), new Bishop(Team.BLACK),
+                new Position(File.B, Rank.SIX), new Pawn(Team.BLACK),
+                new Position(File.E, Rank.SIX), new Queen(Team.BLACK)
+        ));
+        Map<Position, Piece> addedBoard = Map.of(
+                new Position(File.F, Rank.FOUR), new Knight(Team.WHITE),
+                new Position(File.G, Rank.FOUR), new Queen(Team.WHITE),
+                new Position(File.F, Rank.THREE), new Pawn(Team.WHITE),
+                new Position(File.H, Rank.THREE), new Pawn(Team.WHITE),
+                new Position(File.F, Rank.TWO), new Pawn(Team.WHITE),
+                new Position(File.G, Rank.TWO), new Pawn(Team.WHITE),
+                new Position(File.E, Rank.ONE), new Rook(Team.WHITE),
+                new Position(File.F, Rank.ONE), new King(Team.WHITE)
+        );
+        boardMap.putAll(addedBoard);
+        Board board = new Board(boardMap);
+
+        assertAll(
+                () -> assertThat(board.getPawnPositionsOf(Team.BLACK).toList())
+                        .containsExactlyInAnyOrder(
+                                new Position(File.A, Rank.SEVEN),
+                                new Position(File.C, Rank.SEVEN),
+                                new Position(File.B, Rank.SIX)),
+                () -> assertThat(board.getPawnPositionsOf(Team.WHITE).toList())
+                        .containsExactlyInAnyOrder(
+                                new Position(File.F, Rank.THREE),
+                                new Position(File.H, Rank.THREE),
+                                new Position(File.F, Rank.TWO),
+                                new Position(File.G, Rank.TWO)
+                        )
+        );
     }
 }
