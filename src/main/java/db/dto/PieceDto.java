15265d0ba307746920176d7c2e0de1fd859e522d
reddevilmidzy <midzy0228@gmail.com>
2024-03-31 20:06:05 +0900

    refactor: piece 타입 찾는 로직 리팩터링

--- src/main/java/db/dto/PieceDto.java
+++ src/main/java/db/dto/PieceDto.java
@@ -1,43 +1,17 @@
 package db.dto;
 
 import model.Camp;
-import model.piece.Bishop;
-import model.piece.BlackPawn;
-import model.piece.King;
-import model.piece.Knight;
 import model.piece.Piece;
-import model.piece.Queen;
-import model.piece.Rook;
-import model.piece.WhitePawn;
 
 public record PieceDto(String type, String camp) {
 
     public static PieceDto from(final Piece piece) {
-        return new PieceDto(PieceType.findValue(piece), piece.getCamp().toString());
+        return new PieceDto(PieceType.findValue(piece), piece.getCamp().name());
     }
 
-    //TODO 요고 리팩터링
     public Piece convert() {
         final Camp army = find();
-        if ("King".equals(type)) {
-            return new King(army);
-        }
-        if ("Queen".equals(type)) {
-            return new Queen(army);
-        }
-        if ("Knight".equals(type)) {
-            return new Knight(army);
-        }
-        if ("Bishop".equals(type)) {
-            return new Bishop(army);
-        }
-        if ("Rook".equals(type)) {
-            return new Rook(army);
-        }
-        if ("Pawn".equals(type) && army == Camp.WHITE) {
-            return new WhitePawn();
-        }
-        return new BlackPawn();
+        return PieceType.findValue(army, type);
     }
 
     private Camp find() {
--- src/main/java/db/dto/PieceType.java
+++ src/main/java/db/dto/PieceType.java
@@ -1,35 +1,55 @@
 package db.dto;
 
 import java.util.Arrays;
+import model.Camp;
 import model.piece.Bishop;
+import model.piece.BlackPawn;
 import model.piece.King;
 import model.piece.Knight;
-import model.piece.Pawn;
 import model.piece.Piece;
 import model.piece.Queen;
 import model.piece.Rook;
+import model.piece.WhitePawn;
 
 public enum PieceType {
-    KING(King.class, "King"),
-    QUEEN(Queen.class, "Queen"),
-    ROOK(Rook.class, "Rook"),
-    BISHOP(Bishop.class, "Bishop"),
-    KNIGHT(Knight.class, "Knight"),
-    PAWN(Pawn.class, "Pawn");
 
-    private final Class<? extends Piece> clazz;
-    private final String value;
+    WHITE_KING(new King(Camp.WHITE), "King", Camp.WHITE),
+    WHITE_QUEEN(new Queen(Camp.WHITE), "Queen", Camp.WHITE),
+    WHITE_ROOK(new Rook(Camp.WHITE), "Rook", Camp.WHITE),
+    WHITE_BISHOP(new Bishop(Camp.WHITE), "Bishop", Camp.WHITE),
+    WHITE_KNIGHT(new Knight(Camp.WHITE), "Knight", Camp.WHITE),
+    WHITE_PAWN(new WhitePawn(), "Pawn", Camp.WHITE),
+    BLACK_KING(new King(Camp.BLACK), "King", Camp.BLACK),
+    BLACK_QUEEN(new Queen(Camp.BLACK), "Queen", Camp.BLACK),
+    BLACK_ROOK(new Rook(Camp.BLACK), "Rook", Camp.BLACK),
+    BLACK_BISHOP(new Bishop(Camp.BLACK), "Bishop", Camp.BLACK),
+    BLACK_KNIGHT(new Knight(Camp.BLACK), "Knight", Camp.BLACK),
+    BLACK_PAWN(new BlackPawn(), "Pawn", Camp.BLACK);
 
-    PieceType(final Class<? extends Piece> clazz, final String value) {
-        this.clazz = clazz;
-        this.value = value;
+    private final Piece piece;
+    private final String pieceName;
+    private final Camp camp;
+
+    PieceType(final Piece piece, final String pieceName, final Camp camp) {
+        this.piece = piece;
+        this.pieceName = pieceName;
+        this.camp = camp;
     }
 
     public static String findValue(final Piece piece) {
         return Arrays.stream(values())
-                .filter(pieceType -> pieceType.clazz.isInstance(piece))
+                .filter(pieceType1 -> pieceType1.piece.getClass().isInstance(piece))
                 .findFirst()
                 .orElseThrow()
-                .value;
+                .pieceName;
+    }
+
+    public static Piece findValue(final Camp camp, final String type) {
+        return Arrays.stream(values())
+                .filter(pieceType -> pieceType.pieceName.equals(type))
+                .filter(pieceType -> pieceType.camp == camp)
+                .findFirst()
+                .orElseThrow()
+                .piece;
     }
 }
