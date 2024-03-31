66ac4b2375e8cd51b4b5ff589c2217a80c2eb017
libienz <leekh10195@gmail.com>
2024-03-31 16:50:10 +0900

    test: 점수 계산 로직 테스트 추가

--- src/test/java/chess/domain/ChessGameTest.java
+++ src/test/java/chess/domain/ChessGameTest.java
@@ -2,10 +2,15 @@ package chess.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import chess.domain.board.ChessBoard;
 import chess.domain.piece.King;
+import chess.domain.piece.Knight;
+import chess.domain.piece.Pawn;
 import chess.domain.piece.Piece;
+import chess.domain.piece.Rook;
+import chess.domain.piece.Score;
 import chess.domain.piece.Team;
 import chess.domain.position.Position;
 import chess.fixture.PositionFixtures;
@@ -39,5 +44,44 @@ class ChessGameTest {
 
         assertThatThrownBy(() -> chessGame.move(PositionFixtures.A8, PositionFixtures.A7))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("배치된 기물에 맞추어 팀 점수를 계산할 수 있다")
+    @Test
+    void should_CalculateTeamScore() {
+        Map<Position, Piece> board = new HashMap<>();
+        board.put(PositionFixtures.A1, new Knight(Team.WHITE));
+        board.put(PositionFixtures.A8, new Rook(Team.WHITE));
+
+        ChessBoard chessBoard = new ChessBoard(board);
+        ChessGame chessGame = new ChessGame(chessBoard);
+
+        assertThat(chessGame.calculateTeamScore(Team.WHITE)).isEqualTo(new Score(7.5));
+    }
+
+    @DisplayName("폰은 열이 겹치지 않는다면 1점으로 계산한다")
+    @Test
+    void should_CalculatePawnScoreAsOne_When_NoSameFilePawn() {
+        Map<Position, Piece> board = new HashMap<>();
+        board.put(PositionFixtures.A1, new Pawn(Team.WHITE));
+        board.put(PositionFixtures.B1, new Pawn(Team.WHITE));
+
+        ChessBoard chessBoard = new ChessBoard(board);
+        ChessGame chessGame = new ChessGame(chessBoard);
+
+        assertThat(chessGame.calculateTeamScore(Team.WHITE)).isEqualTo(new Score(2));
+    }
+
+    @DisplayName("폰은 열이 겹치지 않는다면 1점으로 계산한다")
+    @Test
+    void should_CalculatePawnScoreAsHalf_When_SameFilePawnExist() {
+        Map<Position, Piece> board = new HashMap<>();
+        board.put(PositionFixtures.A1, new Pawn(Team.WHITE));
+        board.put(PositionFixtures.A2, new Pawn(Team.WHITE));
+
+        ChessBoard chessBoard = new ChessBoard(board);
+        ChessGame chessGame = new ChessGame(chessBoard);
+
+        assertThat(chessGame.calculateTeamScore(Team.WHITE)).isEqualTo(new Score(1));
     }
 }
