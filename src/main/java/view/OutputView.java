23f426092882c25ab177efdbf6381ef1adf2c28e
3juhwan <13selfesteem91@naver.com>
2024-04-01 00:44:11 +0900

    refactor: 모든 출력 뒤에 개행 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,34 +8,39 @@ import java.util.List;
 
 public class OutputView {
     private static final MessageResolver messageResolver = new MessageResolver();
+    public static final String LINE_SEPARATOR = System.lineSeparator();
 
     public static void printGameGuideMessage() {
-        System.out.println(messageResolver.resolveGameStartMessage());
+        printWithLineSeparator(messageResolver.resolveGameStartMessage());
     }
 
     public static void printBoard(ChessBoard board) {
         String boardMessage = messageResolver.resolveBoard(board);
-        System.out.println(boardMessage);
+        printWithLineSeparator(boardMessage);
     }
 
     public static void printScore(Score score) {
         String scoreMessage = messageResolver.resolveScore(score);
-        System.out.println(scoreMessage);
+        printWithLineSeparator(scoreMessage);
     }
 
     public static void printWinner(Score score) {
-        System.out.println(messageResolver.resolveWinner(score));
+        printWithLineSeparator(messageResolver.resolveWinner(score));
     }
 
     public static void printErrorMessage(Exception e) {
-        System.out.println(e.getMessage());
+        printWithLineSeparator(e.getMessage());
     }
 
     public static void printGameRoomGuideMessage(List<RoomDto> rooms) {
-        System.out.println(messageResolver.resolveRoomList(rooms));
+        printWithLineSeparator(messageResolver.resolveRoomList(rooms));
     }
 
     public static void printEnteringRoomMessage(RoomDto roomDto) {
-        System.out.println(messageResolver.resolveEnteringRoomMessage(roomDto));
+        printWithLineSeparator(messageResolver.resolveEnteringRoomMessage(roomDto));
+    }
+
+    private static void printWithLineSeparator(String message) {
+        System.out.println(message + LINE_SEPARATOR);
     }
 }
