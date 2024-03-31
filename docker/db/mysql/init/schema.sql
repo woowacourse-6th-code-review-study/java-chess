7e64983ca318e42100eafd8a44f11169a43b3633
3juhwan <13selfesteem91@naver.com>
2024-03-31 12:39:22 +0900

    feat: room 스키마 추가

--- docker/db/mysql/init/schema.sql
+++ docker/db/mysql/init/schema.sql
@@ -1,14 +1,24 @@
+create table rooms
+(
+    room_id int,
+    primary key (room_id)
+);
+
+create table game_states
+(
+    game_id int,
+    state   varchar(8) not null,
+    primary key (game_id),
+    foreign key (game_id) references rooms (room_id)
+);
+
 create table pieces
 (
     board_file varchar(1) not null,
     board_rank varchar(1) not null,
     color      varchar(8) not null,
     type       varchar(8) not null,
-    primary key (board_file, board_rank)
+    game_id    int,
+    primary key (board_file, board_rank),
+    foreign key (game_id) references rooms (room_id)
 );
-
-create table turns
-(
-    turn varchar(8) not null,
-    primary key (turn)
-)
