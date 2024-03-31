f408e3e236008cd1ffede7da5b3cbc91f82c27a1
kelly6bf <kelly6bf@gmail.com>
2024-03-31 16:43:19 +0900

    refactor: DDL문 업데이트

--- src/main/java/database/init.sql
+++ src/main/java/database/init.sql
@@ -1,15 +1,15 @@
 create table current_player_color
 (
     id bigint auto_increment primary key,
-    player_color varchar(50) not null
+    player_color varchar(10) not null
 );
 
 create table piece
 (
-    type varchar(50) not null,
-    `rank` varchar(50) not null,
-    file varchar(50) not null,
-    color varchar(50) not null,
+    type varchar(10) not null,
+    `rank` varchar(10) not null,
+    file varchar(10) not null,
+    color varchar(10) not null,
     id int auto_increment primary key
 );
 
