b1816b2e1324898f455fb10460d419723520a489
3juhwan <13selfesteem91@naver.com>
2024-04-02 00:24:58 +0900

    fix: GameRoomService 테스트 수정

--- src/test/java/repository/RoomMockDao.java
+++ src/test/java/repository/RoomMockDao.java
@@ -3,47 +3,48 @@ package repository;
 import database.dao.RoomDao;
 import dto.RoomDto;
 import dto.StateDto;
+import dto.UserDto;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
 public class RoomMockDao implements RoomDao {
-    private final Map<Integer, RoomDto> roomRepository;
+    private final Map<RoomDto, UserDto> roomRepository;
     private final Map<Integer, StateDto> turnRepository;
 
-    public RoomMockDao(Map<Integer, RoomDto> roomRepository, Map<Integer, StateDto> turnRepository) {
+    public RoomMockDao(Map<RoomDto, UserDto> roomRepository, Map<Integer, StateDto> turnRepository) {
         this.roomRepository = roomRepository;
         this.turnRepository = turnRepository;
     }
 
-    public void add(final RoomDto roomDto) {
-        roomRepository.put(roomDto.room_id(), roomDto);
+    public void add(UserDto userDto, RoomDto roomDto) {
+        roomRepository.put(roomDto, userDto);
     }
 
-    public Optional<RoomDto> addNewRoom() {
-        if (roomRepository.keySet().isEmpty()) {
-            RoomDto newRoomDto = new RoomDto(1);
-            roomRepository.put(newRoomDto.room_id(), newRoomDto);
-            return Optional.of(newRoomDto);
-        }
-
-        int newRoomId = Collections.max(roomRepository.keySet()) + 1;
-        RoomDto newRoomDto = new RoomDto(newRoomId);
-        roomRepository.put(newRoomDto.room_id(), newRoomDto);
-        return Optional.of(newRoomDto);
+    public Optional<RoomDto> addNewRoom(UserDto userDto) {
+        int newRoomId = roomRepository.keySet()
+                .stream()
+                .mapToInt(RoomDto::room_id)
+                .max()
+                .orElse(0) + 1;
+        RoomDto newRoom = new RoomDto(newRoomId);
+        roomRepository.put(newRoom, userDto);
+        return Optional.of(newRoom);
     }
 
     public Optional<RoomDto> find(final String roomId) {
-        return Optional.of(roomRepository.get(Integer.parseInt(roomId)));
+        return roomRepository.keySet().stream()
+                .filter(room -> room.room_id() == Integer.parseInt(roomId))
+                .findFirst();
     }
 
-    public List<RoomDto> findActiveRoomAll() {
-        return roomRepository.keySet().stream()
-                .filter(roomId -> !Objects.equals(turnRepository.get(roomId).state(), "GAMEOVER"))
-                .map(roomRepository::get)
+    public List<RoomDto> findActiveRoomAll(final UserDto user) {
+        return roomRepository.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(user))
+                .filter(entry -> !Objects.equals(turnRepository.get(entry.getKey().room_id()).state(), "GAMEOVER"))
+                .map(Map.Entry::getKey)
                 .toList();
     }
 }
--- src/test/java/service/GameRoomServiceTest.java
+++ src/test/java/service/GameRoomServiceTest.java
@@ -1,18 +1,18 @@
 package service;
 
+import database.dao.RoomDao;
 import dto.RoomDto;
 import dto.StateDto;
+import dto.UserDto;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import database.dao.RoomDao;
 import repository.RoomMockDao;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -20,11 +20,12 @@ class GameRoomServiceTest {
     private final RoomDto room1 = new RoomDto(1);
     private final RoomDto room2 = new RoomDto(2);
     private final RoomDto room3 = new RoomDto(3);
+    private final UserDto MANGCHO = new UserDto("mangcho");
 
-    private final Map<Integer, RoomDto> roomRepository = Map.of(
-            room1.room_id(), room1,
-            room2.room_id(), room2,
-            room3.room_id(), room3);
+    private final Map<RoomDto, UserDto> roomRepository = Map.of(
+            room1, MANGCHO,
+            room2, MANGCHO,
+            room3, MANGCHO);
 
     private final Map<Integer, StateDto> turnRepository = Map.of(
             room1.room_id(), new StateDto("GAMEOVER", room1.room_id()),
@@ -36,7 +37,7 @@ class GameRoomServiceTest {
         RoomDao roomDao = new RoomMockDao(roomRepository, turnRepository);
         GameRoomService gameRoomService = new GameRoomService(roomDao);
 
-        assertThat(gameRoomService.loadActiveRoomAll(user))
+        assertThat(gameRoomService.loadActiveRoomAll(MANGCHO))
                 .containsExactlyInAnyOrder(room2, room3);
     }
 
@@ -51,11 +52,12 @@ class GameRoomServiceTest {
     }
 
     @Test
-    void 새로운_방을_만든다() {
-        RoomDao roomDao = new RoomMockDao(new HashMap<>(), new HashMap<>());
+    void 세번째_방까지_생성된_시점에_새로_생성한_방은_4번_방이다() {
+        RoomDao roomDao = new RoomMockDao(new HashMap<>(roomRepository), new HashMap<>());
         GameRoomService gameRoomService = new GameRoomService(roomDao);
 
-        assertThatCode(gameRoomService::createNewRoom)
-                .doesNotThrowAnyException();
+        RoomDto newRoom = gameRoomService.createNewRoom(MANGCHO);
+
+        assertThat(newRoom).isEqualTo(new RoomDto(4));
     }
 }
