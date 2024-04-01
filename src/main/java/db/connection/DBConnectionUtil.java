548cfa1638f4273cec48235d137385cc66cd3cc6
reddevilmidzy <midzy0228@gmail.com>
2024-04-01 15:53:17 +0900

    refactor: db 연결 관련된 상수 위치 변경

--- src/main/java/db/connection/DBConnectionUtil.java
+++ src/main/java/db/connection/DBConnectionUtil.java
@@ -1,10 +1,5 @@
 package db.connection;
 
-import static db.connection.ConnectionConst.OPTION;
-import static db.connection.ConnectionConst.PASSWORD;
-import static db.connection.ConnectionConst.SERVER;
-import static db.connection.ConnectionConst.USERNAME;
-
 import constant.ErrorCode;
 import db.exception.ConnectionException;
 import java.sql.Connection;
@@ -13,13 +8,18 @@ import java.sql.SQLException;
 
 public class DBConnectionUtil {
 
+    private static final String SERVER = "localhost:13306";
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
     private DBConnectionUtil() {
     }
 
     public static Connection getConnection(final String database) {
         try {
-            final String url = "jdbc:mysql://" + SERVER.getValue() + "/" + database + OPTION.getValue();
-            return DriverManager.getConnection(url, USERNAME.getValue(), PASSWORD.getValue());
+            final String url = "jdbc:mysql://" + SERVER + "/" + database + OPTION;
+            return DriverManager.getConnection(url, USERNAME, PASSWORD);
         } catch (final SQLException exception) {
             throw new ConnectionException(ErrorCode.CONNECTION);
         }
