f7cf3fff20b4d4640d09d4fa4830619a39be28bf
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-04-01 09:38:57 +0900

    fix(OutputView): 추가 기능 출력

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -29,7 +29,9 @@ public class OutputView {
                 > 체스 게임을 시작합니다.
                 > 게임 시작 : start
                 > 게임 종료 : end
-                > 게임 이동 : move source위치 target위치 - 예. move b2 b3""");
+                > 게임 이동 : move source위치 target위치 - 예. move b2 b3
+                > 팀 별 기물 점수 조회 : status
+                """);
     }
 
     public void printCurrentTurn(Team turn) {
