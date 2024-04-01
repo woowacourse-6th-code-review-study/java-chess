426d51d196900e2eb8ce72e83a81355e963f1407
hyxrxn <gpfls981220@gmail.com>
2024-04-01 17:24:37 +0900

    refactor(BoardDao): 메서드 분리

--- src/main/java/chess/BoardDao.java
+++ src/main/java/chess/BoardDao.java
@@ -11,6 +11,7 @@ import chess.dto.BoardDto;
 import chess.dto.PieceDto;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -58,16 +59,21 @@ public class BoardDao {
         final var query = "INSERT INTO board VALUES(?, ?, ?, ?, ?)";
         try (final var connection = getConnection();
              final var preparedStatement = connection.prepareStatement(query)) {
-            for (Entry<Position, PieceDto> pieceEntry : board.entrySet()) {
-                preparedStatement.setInt(1, gameId);
-                preparedStatement.setString(2, pieceEntry.getKey().getFile());
-                preparedStatement.setString(3, pieceEntry.getKey().getRank());
-                preparedStatement.setString(4, pieceEntry.getValue().getType());
-                preparedStatement.setString(5, pieceEntry.getValue().getTeam());
-                preparedStatement.executeUpdate();
-            }
+            addPiece(gameId, board, preparedStatement);
         } catch (final SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void addPiece(int gameId, Map<Position, PieceDto> board, PreparedStatement preparedStatement)
+            throws SQLException {
+        for (Entry<Position, PieceDto> pieceEntry : board.entrySet()) {
+            preparedStatement.setInt(1, gameId);
+            preparedStatement.setString(2, pieceEntry.getKey().getFile());
+            preparedStatement.setString(3, pieceEntry.getKey().getRank());
+            preparedStatement.setString(4, pieceEntry.getValue().getType());
+            preparedStatement.setString(5, pieceEntry.getValue().getTeam());
+            preparedStatement.executeUpdate();
         }
     }
 
@@ -109,16 +115,32 @@ public class BoardDao {
         try (final var connection = getConnection();
              final var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, gameId);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    String turn = resultSet.getString("turn");
-                    return Team.valueOf(turn.toUpperCase());
-                }
-            }
+            Team turn = getTeam(preparedStatement);
+            validateExistTeam(turn);
+            return turn;
         } catch (SQLException e) {
             throw new RuntimeException("게임 턴 정보 조회 실패", e);
         }
-        throw new IllegalArgumentException("해당 게임 ID를 찾을 수 없습니다.");
+    }
+
+    private Team getTeam(PreparedStatement preparedStatement) throws SQLException {
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            return getTeam(resultSet);
+        }
+    }
+
+    private Team getTeam(ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            String turn = resultSet.getString("turn");
+            return Team.valueOf(turn.toUpperCase());
+        }
+        return null;
+    }
+
+    private void validateExistTeam(Team turn) {
+        if (turn == null) {
+            throw new IllegalArgumentException("해당 게임 ID를 찾을 수 없습니다.");
+        }
     }
 
     private Map<Position, Piece> getBoardPieces(int gameId) {
@@ -127,38 +149,53 @@ public class BoardDao {
         try (final var connection = getConnection();
              final var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, gameId);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    File file = File.valueOf(resultSet.getString("file").toUpperCase());
-                    Rank rank = Rank.valueOf(resultSet.getString("rank").toUpperCase());
-                    PieceType type = PieceType.valueOf(resultSet.getString("type").toUpperCase());
-                    Team team = Team.valueOf(resultSet.getString("team").toUpperCase());
-                    Position position = new Position(file, rank);
-                    Piece piece = type.createPiece(team);
-                    pieces.put(position, piece);
-                }
-            }
+            getOnePiece(preparedStatement, pieces);
         } catch (SQLException e) {
             throw new RuntimeException("보드 정보 조회 실패", e);
         }
         return pieces;
     }
 
+    private void getOnePiece(PreparedStatement preparedStatement, Map<Position, Piece> pieces) throws SQLException {
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            getOnePiece(pieces, resultSet);
+        }
+    }
+
+    private void getOnePiece(Map<Position, Piece> pieces, ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            File file = File.valueOf(resultSet.getString("file").toUpperCase());
+            Rank rank = Rank.valueOf(resultSet.getString("rank").toUpperCase());
+            PieceType type = PieceType.valueOf(resultSet.getString("type").toUpperCase());
+            Team team = Team.valueOf(resultSet.getString("team").toUpperCase());
+            Position position = new Position(file, rank);
+            Piece piece = type.createPiece(team);
+            pieces.put(position, piece);
+        }
+    }
+
     public boolean isExistBoard(int gameId) {
         final String query = "SELECT EXISTS(SELECT 1 FROM game WHERE gameId = ?) AS Exist";
-        boolean exists = false;
         try (final var connection = getConnection();
              final var preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, gameId);
-            try (ResultSet rs = preparedStatement.executeQuery()) {
-                if (rs.next()) {
-                    exists = rs.getBoolean("Exist");
-                }
-                return exists;
-            }
+            return getExist(preparedStatement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return exists;
+        return false;
+    }
+
+    private boolean getExist(PreparedStatement preparedStatement) throws SQLException {
+        try (ResultSet rs = preparedStatement.executeQuery()) {
+            return getExist(rs);
+        }
+    }
+
+    private boolean getExist(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            return rs.getBoolean("Exist");
+        }
+        return false;
     }
 }
